d06eb345d19b0a2e1fa91aebada16caf3da10267
Amy Paschke <pasch049@umn.edu>
2020-10-26 12:49:15 -0500

    added ability to check the current hour

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,6 @@
 /*GIVEN I am using a daily planner to create a schedule
-WHEN I scroll down
-THEN I am presented with time blocks for standard business hours
 WHEN I view the time blocks for that day
 THEN each time block is color-coded to indicate whether it is in the past, present, or future
-WHEN I click into a time block
-THEN I can enter an event
 WHEN I click the save button for that time block
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
@@ -16,27 +12,101 @@ let saveButton = document.getElementById("saveButtons");
 
 //sets the date with the help of Moment.js
 let today = moment();
-currentDay.textContent = today.format("MM-DD-YYYY");
+currentDay.textContent = today.format("MM-DD-YYYY, LT");
 
-/*
-//array used to loop through the times of the day
-let hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-let hoursOfDay = document.getElementById("hours");
+//pulls the current hour
+let currentHour = moment().hour();
 
-//adds hours to the left side of the page
-for (let i = 0; i < hours.length; i++) {
-    let hoursDiv = document.createElement("div");
-    hoursDiv.textContent = hours[i];
-    hoursOfDay.appendChild(hoursDiv);
-    hoursDiv.setAttribute("class", "hour time-block");
+//checks the current time
+/*window.setInterval(updateTime, 10000);
+
+function updateTime() {
+
+}*/
+
+let hours = {
+  9: "9am",
+  10: "10am",
+  11: "11am",
+  12: "12pm",
+  13: "1pm",
+  14: "2pm",
+  15: "3pm",
+  16: "4pm",
+  17: "5pm"
 }
-*/
 
-/*listEl.addEventListener("click", function(event) {
-  event.preventDefault();
-  if(event.target.matches("button")) {
-    let item = document.createElement("div");
-    item.textContent = groceries[event.target.parentElement.id];
-    shoppingCartEl.append(item);
+for (let t = 0; t < hours.length; t++) {
+  if (currentHour > hours[t]) {
+    
   }
-});*/
+  
+}
+
+
+
+/* function handleClick(event) {
+  if (event.target.matches("button")) {
+    event.preventDefault();
+    modalEl.style.display = "block";
+    currentId = parseInt(event.target.parentElement.id); //id of this modal box is taken from the id of the parent element at the time of the click
+    let name = people[currentId].name;
+    let description = people[currentId].description; //where do we define "description?"
+    modalNameEl.textContent = name;
+    if(description) {
+      descriptionEl.value = description;
+    } else {
+      descriptionEl.value = "";
+    }
+  }
+} */
+
+let times = {
+  nineAM: document.getElementById("row1"),
+  tenAM: document.getElementById("row2"),
+  elevenAM: document.getElementById("row3"),
+  twelvePM: document.getElementById("row4"),
+  onePM: document.getElementById("row5"),
+  twoPM: document.getElementById("row6"),
+  threePM: document.getElementById("row7"),
+  fourPM: document.getElementById("row8"),
+  fivePM: document.getElementById("row9")
+}
+console.log(times);
+
+saveButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  // create user object from submission
+  let save = {
+    firstName: firstNameInput.value.trim(),
+    lastName: lastNameInput.value.trim(),
+    email: emailInput.value.trim(),
+    password: passwordInput.value.trim()
+  };
+
+  console.log(user);
+  
+  // validate the fields
+  if (user.firstName === "") {
+    displayMessage("error", "First name cannot be blank");
+  } else if (user.lastName === "") {
+    displayMessage("error", "Last name cannot be blank");
+  } else if (user.email === "") {
+    displayMessage("error", "Email cannot be blank");
+  } else if (user.password === "") {
+    displayMessage("error", "Password cannot be blank");
+  } else {
+    displayMessage("success", "Registered successfully");
+
+    // set new submission
+    localStorage.setItem("user", JSON.stringify(user));
+    
+    // get most recent submission
+    var lastUser = JSON.parse(localStorage.getItem("user"));
+    userFirstNameSpan.textContent = lastUser.firstName;
+    userLastNameSpan.textContent = lastUser.lastName;
+    userEmailSpan.textContent = lastUser.email;
+    userPasswordSpan.textContent = lastUser.password;
+  }
+});
